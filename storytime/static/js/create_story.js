234fa3d89e4f2e601e1ad9eb02d97873b0e18407
valehelle <hazmiirfan92@gmail.com>
2015-11-24 20:26:29 +0800

    Update design
    New update

--- storytime/static/js/create_story.js
+++ storytime/static/js/create_story.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	var position = 1;
 	$("#addimage").click(function(event){
 		var string = "onchange = \"$('#preview" + position + "')[0].src = window.URL.createObjectURL(this.files[0])\"";
-		$("<div class = \"col-xs-12 col-sm-12  col-md-4 col-lg-4 item\" style = \"overflow: hidden;\"><label for=\"id_source\">Image:</label><input id=\"id_source\" name=\"source\" type=\"file\" " + string +  " ><br><img id=\"preview" + position + "\" style=\"\"  class = \"img-responsive\"/> <input id=\"id_position\" name=\"position\" type=\"hidden\" value = " + position + "></div>").insertBefore("#choice");
+		$("<div class = \"col-xs-12 col-sm-12  col-md-4 col-lg-4 item\" style = \"overflow: hidden;\"><label for=\"id_source\">Image:</label><input id=\"id_source\" name=\"source\" type=\"file\" " + string +  " ><br><img id=\"preview" + position + "\" style=\"max-height:300px;\"  class = \"img-responsive\"/> <input id=\"id_position\" name=\"position\" type=\"hidden\" value = " + position + "></div>").insertBefore("#choice");
 		if(position%3 == 0)
 			$("<div class = \"col-xs-12 col-sm-12 col-md-12 col-lg-12\"><br></div>").insertBefore("#choice");
 		position++;
